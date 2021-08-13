5949ff176582e01ab1d48effea1b04a46ed7a623
whitecalvin <whitecalvin@gxsoft.co.kr>
2021-08-13 12:38:46 +0900

    Project completed

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
+
+const TodoListBlock = styled.div`
+    flex: 1;
+    padding: 20px 32px;
+    padding-bottom: 48px;
+    overflow-y: auto;
+    // background: gray; /* 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
+`;
+
+const TodoList = () => {
+    const todos = useTodoState();
+    return (
+        <TodoListBlock>
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    id={todo.id}
+                    text={todo.text}
+                    done={todo.done}
+                />
+            ))}
+        </TodoListBlock>
+    );
+};
+
+export default TodoList;
